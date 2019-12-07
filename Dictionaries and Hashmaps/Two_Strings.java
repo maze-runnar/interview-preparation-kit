82ed1bb1712dfb10e6cc27ed6272c9ff1da4659e
Aatmik Jain <aatmikjain1803@gmail.com>
2019-12-07 13:22:35 +0530

    Revert "Revert "added java codes""
    This reverts commit 87d0cb4cba9a3848bd970ed963cdac2cfa6d2120.

---
+++ Dictionaries and Hashmaps/Two_Strings.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        for(char c : "abcdefghijklmnopqrstuvwxyz".toCharArray())
+        {
+            if(s1.indexOf(c)>-1 && s2.indexOf(c)>-1)
+                return "YES";
+        }
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
