7764dde887c985c1b2dbbd5add73c9805f3cfbd4
Aatmik Jain <aatmikjain1803@gmail.com>
2019-12-12 11:08:34 +0530

    java solutions for warm-up challenges

---
+++ Warm-Up Challenges/countingValleys.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s)
+    {
+        int count=0, cur=0,prev=0;
+        for(int i=0; i<n; i++)
+        {
+            prev=cur;
+            if(s.charAt(i)=='D')
+            {
+                cur--;
+                if(prev==0)
+                    count++;
+            }
+            else
+            {
+                cur++;
+            }
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ Warm-Up Challenges/repeatedString.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long count=0;
+        int len = s.length();
+        long mul = n/len;
+        long mod = n%len;
+        long add=0;
+        for(int i=0; i<len; i++)
+        {
+            if(i==mod && mod!=0)
+                add=count;
+            if(s.charAt(i)=='a')
+                count++;
+        }
+        count*=mul;
+        count+=add;
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
