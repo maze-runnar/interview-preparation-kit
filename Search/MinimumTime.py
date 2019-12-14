f15ca7877e9fb2d1f4052c6af586e9d48f7ba2e5
Rajat Agrawal <45696541+RashakDude@users.noreply.github.com>
2019-12-15 00:45:58 +0530

    Updated the code as per Geeks for Geeks

--- Search/MinimumTime.py
+++ Search/MinimumTime.py
@@ -6,32 +6,28 @@ import random
 import re
 import sys
 
+def findItems(machines,temp):
+    ans = 0
+    for i in range(len(machines)):
+        ans += temp // machines[i]
+    return ans
+
+def BinarySearch(machines,goal,high):
+    low = 1
+    while low < high:
+        mid = (low + high) >> 1
+        item = findItems(machines,mid)
+        if item < goal:
+            low = mid + 1
+        else:
+            high = mid
+    return high
 # Complete the minTime function below.
 def minTime(machines, goal):
-    machines.sort()
-
-    low_rate = machines[0]
-    lower_bound = (goal // (len(machines) / low_rate))
-    high_rate = machines[-1]
-    upper_bound = (goal // (len(machines) / high_rate)) + 1
-
-    while lower_bound < upper_bound:
-
-        num_days = (lower_bound + upper_bound) // 2
-        total = getNumItems(machines, goal, num_days)
-        if total >= goal:
-            upper_bound = num_days
-        else:
-            lower_bound = num_days + 1
-
-    return int(lower_bound)
-
-
-def getNumItems(machines, goal, num_days):
-    total = 0
-    for machine in machines:
-        total += (num_days // machine)
-    return total
+    maxval = -sys.maxsize 
+    for i in range(len(machines)): 
+        maxval = max(maxval, machines[i]) 
+    return BinarySearch(machines,goal,maxval * goal)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
