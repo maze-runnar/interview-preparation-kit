81ca743bdebc65ae33a4a5384da6c5dd67b66187
WinterSoldier <ayushsingh1315@gmail.com>
2019-12-19 05:41:50 -0500

    Added Fibbonaci_Numbers.py
    Compute Fibbonaci value at 'n' using recursion in Python3.

---
+++ Recursion and Backtracking/Fibbonaci_numbers.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+   
+
+n = int(input())
+print(fibonacci(n))
+
