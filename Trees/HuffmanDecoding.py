d8cb7694fcf5fd423007709b7635b38eee799fb3
Rajat Agrawal <45696541+RashakDude@users.noreply.github.com>
2019-12-21 15:54:06 +0530

    Added questions from Trees in Python

---
+++ Trees/HuffmanDecoding.py
@@ -0,0 +1,80 @@
+import queue as Queue
+
+cntr = 0
+
+class Node:
+    def __init__(self, freq, data):
+        self.freq = freq
+        self.data = data
+        self.left = None
+        self.right = None
+        global cntr
+        self._count = cntr
+        cntr = cntr + 1
+        
+    def __lt__(self, other):
+        if self.freq != other.freq:
+            return self.freq < other.freq
+        return self._count < other._count
+
+def huffman_hidden():#builds the tree and returns root
+    q = Queue.PriorityQueue()
+
+    
+    for key in freq:
+        q.put((freq[key], key, Node(freq[key], key) ))
+    
+    while q.qsize() != 1:
+        a = q.get()
+        b = q.get()
+        obj = Node(a[0] + b[0], '\0' )
+        obj.left = a[2]
+        obj.right = b[2]
+        q.put((obj.freq, obj.data, obj ))
+        
+    root = q.get()
+    root = root[2]#contains root object
+    return root
+
+def dfs_hidden(obj, already):
+    if(obj == None):
+        return
+    elif(obj.data != '\0'):
+        code_hidden[obj.data] = already
+        
+    dfs_hidden(obj.right, already + "1")
+    dfs_hidden(obj.left, already + "0")
+
+"""class Node:
+    def __init__(self, freq,data):
+        self.freq= freq
+        self.data=data
+        self.left = None
+        self.right = None
+"""        
+def is_last(node):
+    return not node.left and not node.right
+
+def go_right(node):
+    return node.right
+
+def go_left(node):
+    return node.left
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def decodeHuff(root , s):
+    result = []
+    node = root
+    for char in s:
+        if char == '1':
+            node = go_right(node)
+        elif char == '0':
+            node = go_left(node)
+        
+        if is_last(node):
+            result.append(node.data)
+            node = root
+            
+    print(''.join(result))
+
+ip = input()
---
+++ Trees/LowestCommonAncestor.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+def lca(root, v1, v2):
+    while(root != None):
+        if(v1 > root.info and v2 > root.info):
+            root = root.right
+        elif(v1 < root.info and v2 < root.info):
+            root = root.left
+        else:
+            break
+    return root
+  #Enter your code here
+
+tree = BinarySearchTree()
