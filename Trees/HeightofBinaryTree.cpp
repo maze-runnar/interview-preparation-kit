72d962a1b040a8284634104436780ee1e80e3c3f
Akash Kumar Singhal <akashksinghalnewdelhi@gmail.com>
2019-12-26 01:17:40 +0530

    Added Height Of Binary Tree

---
+++ Trees/HeightofBinaryTree.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+    int height(Node *root, int calls){
+        if (root == NULL)
+            return calls-1;
+        return max(height(root->left, calls+1), height(root->right, calls+1));
+    }
+    int height(Node* root) {
+        return height(root, 0);
+    }
+
+}; 
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
