0317e9eff4ffd6d89b93f8545e55b1e436bcbef1
WinterSoldier <ayushsingh1315@gmail.com>
2019-12-26 00:51:25 -0500

    Created fibonacci_numbers.java

---
+++ Recursion and Backtracking/fibonacci_numbers.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int fibonacci(int n) {
+
+
+        if(n==0)
+        {
+            return 0;
+        }
+        else if (n==1)
+        {
+            return 1;
+
+        }
+
+        else{
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
