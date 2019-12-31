66614ca00075cdb2814818c68824b1af5bff179c
Deepak Chauhan <deepakchauhan878@gmail.com>
2019-12-31 01:49:00 -0800

    Added solution for Minimum Absolute Difference Array in Greedy Algo section in JAVA

---
+++ Greedy Algorithm/MinimumAbsoluteDifferenceInAnArray.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumAbsoluteDifference function below.
+    static int minimumAbsoluteDifference(int[] arr) {
+        Arrays.sort(arr);
+        int min = 1000000000;
+        for(int i =0; (i+1)<arr.length; i++){
+            if(Math.abs(arr[i] - arr[i+1])<min){
+                min = Math.abs(arr[i] - arr[i+1]);
+            }
+        }
+        return min;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
