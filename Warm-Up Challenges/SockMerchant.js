dcfd52c21f2999ef47135bd7add27b7b10fc4c1f
alpha_numeric <35887813+SREEHARI-M-S@users.noreply.github.com>
2020-01-02 01:12:38 +0530

    Created SockMerchant.js

---
+++ Warm-Up Challenges/SockMerchant.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function sockMerchant(n, ar) {
+    var x = 0;
+    ar.sort();
+    for(let i=0; i<n;i++){
+        if(ar[i] == ar[i+1]){
+            i++;
+                  x++;
+           }
+    }
+return x;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
