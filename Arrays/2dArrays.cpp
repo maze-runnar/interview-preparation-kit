67e41b1acbbd5ef65d21f3cfe87e8904b1fc26f2
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-09-03 19:10:56 +0530

    Update 2dArrays.cpp

--- Arrays/2dArrays.cpp
+++ Arrays/2dArrays.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++)
+    {
+       for(int arr_j = 0;arr_j < 6;arr_j++)
+       {
+          cin >> arr[arr_i][arr_j];
+       }
+    }
+    int maxsum=-64;  
+    int hoursum;   
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            hoursum=arr[i+1][j+1];    
+            for(int k=0;k<3;k++)
+                {                   
+                    hoursum = hoursum + arr[i][j+k] + arr[i+2][j+k]; 
+                }             
+            if(hoursum > maxsum)         
+                maxsum = hoursum;       
+        }      
+    }     
+    cout<<maxsum;
+    return 0;
+}
 
